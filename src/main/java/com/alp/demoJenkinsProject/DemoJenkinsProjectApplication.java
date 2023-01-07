2eb6dfc60be61d6a63c4d7c0fc8b6b56d75b2f9c
AlpBugraCelik <alp.bugra@aerlingus.com>
2023-01-07 13:58:54 +0300

    first commit

---
+++ src/main/java/com/alp/demoJenkinsProject/DemoJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alp.demoJenkinsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alp/demoJenkinsProject/DemoJenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alp.demoJenkinsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
